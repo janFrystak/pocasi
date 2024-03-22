5e612024e0ade3063ea1ffbc493b83c62bb3d261
janFrystak <janfrystak@gmail.com>
2024-03-22 10:42:45 +0100

    final push

--- app/Views/StaniceInfo.php
+++ app/Views/StaniceInfo.php
@@ -2,7 +2,10 @@
 echo $this->extend("layout/template");
 echo $this->section("content"); ?>
 <h1>Přehled meteorologických stanic</h1>
-<?php foreach ($bundesland as $stanice) : ?>
+<?php 
+
+$cesta = "pic/vlajky/";
+foreach ($bundesland as $stanice) : ?>
 
 <div class="container-fluid">
     <div class ="row">
@@ -11,7 +14,7 @@ echo $this->section("content"); ?>
   <h4 class="card-title"><?= $stanice->place ?></h4>
     <p class="card-text">Zeměpisná výška: <?= $stanice->geo_latitude ?></p>
     <p class="card-text">Zeměpisná šířka: <?= $stanice->geo_longtitude ?></p>
-    <p class="card-text">Zeměpisná šířka: <?= $stanice->flag ?></p>
+    <img class="card-img" width="400" height="300"> <?php echo img ($cesta.$stanice->flag);?></img>
   
   
    
